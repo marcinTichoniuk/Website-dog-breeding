d77c142914dd771d554ba187d1185bb2572e028d
Marcin Tichoniuk <marcin.tichoniuk1@gmail.com>
2019-08-07 14:03:27 +0200

    Updates

---
+++ precache-manifest.bdc85c00940faac3bcf729bd1eac24e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec17c32a5213b47ece8493272066721",
+    "url": "/Website-dog-breeding/index.html"
+  },
+  {
+    "revision": "3426e74f052601c85047",
+    "url": "/Website-dog-breeding/static/css/2.f74a8007.chunk.css"
+  },
+  {
+    "revision": "fba2b7b4f478fdb13dc4",
+    "url": "/Website-dog-breeding/static/css/main.9e97b940.chunk.css"
+  },
+  {
+    "revision": "3426e74f052601c85047",
+    "url": "/Website-dog-breeding/static/js/2.2f42fd4a.chunk.js"
+  },
+  {
+    "revision": "fba2b7b4f478fdb13dc4",
+    "url": "/Website-dog-breeding/static/js/main.c5c45861.chunk.js"
+  },
+  {
+    "revision": "9826a0ef631894f95c2b",
+    "url": "/Website-dog-breeding/static/js/runtime~main.d1508c8c.js"
+  },
+  {
+    "revision": "1b546818e18b1d2f8b93588db0f65830",
+    "url": "/Website-dog-breeding/static/media/G3.1b546818.jpg"
+  },
+  {
+    "revision": "8a749cc766ea4c1c5b8a48fabf2a0bb7",
+    "url": "/Website-dog-breeding/static/media/aris.8a749cc7.jpg"
+  },
+  {
+    "revision": "7cf0d9df0d52025c6dae70854e435005",
+    "url": "/Website-dog-breeding/static/media/arisM.7cf0d9df.jpg"
+  },
+  {
+    "revision": "8e19a87ee32901a3a5957d292af76e13",
+    "url": "/Website-dog-breeding/static/media/borderBack.8e19a87e.jpg"
+  },
+  {
+    "revision": "1545a80fc31cc79cb5bf3337e920f7de",
+    "url": "/Website-dog-breeding/static/media/fredy.1545a80f.jpg"
+  },
+  {
+    "revision": "a85e324b211c65edba408953747862ed",
+    "url": "/Website-dog-breeding/static/media/fredyM.a85e324b.jpg"
+  },
+  {
+    "revision": "f1bbe8b581c931837d2b3c510a425136",
+    "url": "/Website-dog-breeding/static/media/kora1.f1bbe8b5.jpg"
+  }
+]);
