3c400c005a8415f29a6ec055066b69279f18ca11
Marcin Tichoniuk <marcin.tichoniuk1@gmail.com>
2019-08-13 10:49:32 +0200

    add componentDidMount function

--- src/pages/DogPage.js
+++ src/pages/DogPage.js
@@ -2,51 +2,65 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import ImageGallery from 'react-image-gallery';
-// import "react-image-gallery/styles/css/image-gallery.css";
 
 import file from '../data/data';
 
 // @TODO change dog-lists? (make this as a card)
 
-const DogPage = props => {
-  // console.log(props)
-  const chooseGallery = (dogName) => {
-    const chosenDog = file.dogs.filter(dog => dog.name === dogName);
-
-    const chosenDogInfo = chosenDog.map(dog => (
-      <div key={dog.id} className="page__dog">
-        <h3 className="page__title">{dog.name}</h3>
-        <p className="page__description">{dog.description}</p>
-        <p className="page__description">Data ur. {dog.birth}</p>
-        <div className="page__lists">
-          <p className="page__description">Badania: </p>
-          <ul className="page__list">
-            <li className="page__list-element">{dog.exams}</li>
-          </ul >
-          {dog.titles ?
-            <>
-              <p className="page__description">Tytuły: </p>
-              <ul className="page__list">
-                <li className="page__list-element">{dog.titles}</li>
-              </ul>
-            </> : null}
-        </div>
-        <div className="page__gallery">
-          <ImageGallery items={dog.gallery} />
-        </div>
-      </div>
-    ))
-    return chosenDogInfo;
+class DogPage extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    birth: '',
+    exams: '',
+    titles: '',
+    gallery: [],
   }
 
-  return (
-    <>
-      {chooseGallery(props.match.params.id)}
-      <Link to="/dogs" className="page__link">
-        <button className="btn page__btn">Wróć</button>
-      </Link>
-    </>
-  );
+  componentDidMount() {
+    let id = this.props.match.params.id;
+    const dog = file.dogs.find(dog => dog.name === id);
+    this.setState({
+      name: dog.name,
+      description: dog.description,
+      birth: dog.birth,
+      exams: dog.exams,
+      titles: dog.titles,
+      gallery: dog.gallery,
+    })
+  }
+
+  render() {
+    const { name, description, birth, exams, titles, gallery } = this.state;
+    return (
+      <>
+        <div className="page__dog">
+          <h3 className="page__title">{name}</h3>
+          <p className="page__description">{description}</p>
+          <p className="page__description">Data ur. {birth}</p>
+          <div className="page__lists">
+            <p className="page__description">Badania: </p>
+            <ul className="page__list">
+              <li className="page__list-element">{exams}</li>
+            </ul >
+            {titles ?
+              <>
+                <p className="page__description">Tytuły: </p>
+                <ul className="page__list">
+                  <li className="page__list-element">{titles}</li>
+                </ul>
+              </> : null}
+          </div>
+          <div className="page__gallery">
+            <ImageGallery items={gallery} />
+          </div>
+        </div>
+        <Link to="/dogs" className="page__link">
+          <button className="btn page__btn">Wróć</button>
+        </Link>
+      </>
+    );
+  }
 }
 
 export default DogPage;
--- src/pages/FemaleDogPage.js
+++ src/pages/FemaleDogPage.js
@@ -1,49 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import ImageGallery from 'react-image-gallery';
 
 import file from '../data/data';
 
-const FemaleDogPage = props => {
-
-  const chooseGallery = (dogName) => {
-    const chosenDog = file.femaleDogs.filter(dog => dog.name === dogName);
-
-    const chosenDogInfo = chosenDog.map(dog => (
-      <div key={dog.id} className="page__dog">
-        <h3 className="page__title">{dog.name}</h3>
-        <p className="page__description">{dog.description}</p>
-        <p className="page__description">Data ur. {dog.birth}</p>
-        <div className="page__lists">
-          <p className="page__description">Badania: </p>
-          <ul className="page__list">
-            <li className="page__list-element">{dog.exams}</li>
-          </ul >
-          {dog.titles ?
-            <>
-              <p className="page__description">Tytuły: </p>
-              <ul className="page__list">
-                <li className="page__list-element">{dog.titles}</li>
-              </ul>
-            </> : null}
-        </div>
-        <div className="page__gallery">
-          <ImageGallery items={dog.gallery} />
-        </div>
-      </div>
-    ))
-    return chosenDogInfo;
+class FemaleDogPage extends Component {
+  state = {
+    name: '',
+    description: '',
+    birth: '',
+    exams: '',
+    titles: '',
+    gallery: [],
   }
 
-  return (
-    <>
-      {chooseGallery(props.match.params.id)}
-      <Link to="/femaleDogs" className="page__link">
-        <button className="btn page__btn">Wróć</button>
-      </Link>
-    </>
-  );
+  componentDidMount() {
+    let id = this.props.match.params.id;
+    const dog = file.femaleDogs.find(dog => dog.name === id);
+    this.setState({
+      name: dog.name,
+      description: dog.description,
+      birth: dog.birth,
+      exams: dog.exams,
+      titles: dog.titles,
+      gallery: dog.gallery,
+    })
+  }
+
+  render() {
+    const { name, description, birth, exams, titles, gallery } = this.state;
+    return (
+      <>
+        <div className="page__dog">
+          <h3 className="page__title">{name}</h3>
+          <p className="page__description">{description}</p>
+          <p className="page__description">Data ur. {birth}</p>
+          <div className="page__lists">
+            <p className="page__description">Badania: </p>
+            <ul className="page__list">
+              <li className="page__list-element">{exams}</li>
+            </ul >
+            {titles ?
+              <>
+                <p className="page__description">Tytuły: </p>
+                <ul className="page__list">
+                  <li className="page__list-element">{titles}</li>
+                </ul>
+              </> : null}
+          </div>
+          <div className="page__gallery">
+            <ImageGallery items={gallery} />
+          </div>
+        </div>
+        <Link to="/femaleDogs" className="page__link">
+          <button className="btn page__btn">Wróć</button>
+        </Link>
+      </>
+    );
+  }
 }
 
 export default FemaleDogPage;
